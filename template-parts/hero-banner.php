c2ff4392052ee4199a9fdac53a4cfbb7b16260cf
Magalie Chetrit <1594036+BluePraise@users.noreply.github.com>
2026-02-21 15:12:02 -0500

    improve readability by adding spacing and adjusting background image handling

--- template-parts/hero-banner.php
+++ template-parts/hero-banner.php
@@ -5,11 +5,14 @@
  * @package ErikKorte
  */
 
+
+// Banner image is set via Featured Image in the page editor.
 $banner_image = get_the_post_thumbnail_url(get_the_ID(), 'full');
+
 $banner_title = isset($args['title']) ? $args['title'] : get_the_title();
 ?>
 
-<div class="hero-banner" style="background-image: url(<?php echo esc_url($banner_image); ?>);">
+<div class="hero-banner"<?php if ($banner_image) : ?> style="background-image: url(<?php echo esc_url($banner_image); ?>);"<?php endif; ?>>
     <div class="container">
         <h1 class="banner-heading"><?php echo esc_html($banner_title); ?></h1>
     </div>
